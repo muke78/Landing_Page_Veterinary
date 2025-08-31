104b1fd442fe3e0d6eb3fec7eacab04ecc57d741
Erick Muke <muke7881@gmail.com>
2025-08-31 06:02:14 -0600

    feat(setup): COnfiguracion inicial de la pagina y algunas pruebas con el entorno vue

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { Button } from "@/components/ui/button"
+
+const count = ref(0)
+
+const incrementCount = () => {
+  count.value++
+}
+
+</script>
+
+<template>
+  <div>
+    <Button variant="secondary" @click="incrementCount">
+      Count is: {{ count }}
+    </Button>
+  </div>
+</template>
